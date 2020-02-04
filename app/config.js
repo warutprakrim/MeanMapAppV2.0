9beb5f8432c1006ea986cb4950b87fb0cd7c255e
warut prakrim <warut.prakrim@accenture.com>
2020-02-04 10:09:44 +0700

    init

--- app/config.js
+++ app/config.js
@@ -2,6 +2,13 @@
 
 module.exports = {
 
+    bitnami:
+    {
+        name: "MongoDB Service",
+        url: "mongodb://" + process.env.DATABASE_USERNAME + ":" + process.env.DATABASE_PASSWORD + "@" + process.env.DATABASE_HOST + "/" + process.env.DATABASE_NAME,
+        port: process.env.DATABASE_PORT
+    },
+
     mongolab:
     {
         name: "mongolab",
